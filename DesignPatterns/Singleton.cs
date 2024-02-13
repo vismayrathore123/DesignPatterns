9649f46601925b07eba71715f82169552515f05a
vismayrathore123 <HOME@DESKTOP-EAS70TG>
2024-02-13 18:19:56 +0530

    Add project files.

---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+        private static Singleton instance;
+        public static Singleton getInstance()
+        {
+            if(instance == null)
+            {
+                instance= new Singleton();
+            }
+            return instance;
+        }
+        static void Main(string[] args)
+        {
+        Singleton sc = Singleton.getInstance();
+        }
+    }
+    
+    
+}
---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,52 @@
+﻿using DesignPatterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    public class ConcreteProductA: IProduct
+    {
+        public void operation()
+        {
+            Console.WriteLine("Hello ConcereteProduct A ");
+        }
+    }
+    public class ConcreteProductB: IProduct
+    {
+        public void operation()
+        {
+            Console.WriteLine("Hello concreteProduct B");
+        }
+    }
+    public class ProductFactory
+    {
+        public IProduct CreateProduct  (int type)
+        {
+            switch (type)
+            {
+                case 0: 
+                    return new ConcreteProductA();
+                case 1: 
+                    return new ConcreteProductB();
+                default:
+                    throw new ArgumentException("invalid product type ");
+            }
+        }
+    }
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductFactory fck= new ProductFactory();   
+            IProduct productA= fck.CreateProduct (1);
+            IProduct productB=fck.CreateProduct (0);
+
+            productA.operation();
+            productB.operation();
+        }
+    }
+}
